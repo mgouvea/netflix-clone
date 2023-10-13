1611ec2f95e8ead0eb2fd508d7f55589fdf9f372
Mateus Gouvêa <falamateus.gouvea@gmail.com>
2023-10-13 15:39:17 -0300

    swr

---
+++ pages/api/current.ts
@@ -0,0 +1,20 @@
+import serverAuth from '@/lib/serverAuth';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== 'GET') {
+    res.status(405).end();
+  }
+
+  try {
+    const { currentUser } = await serverAuth(req);
+
+    return res.status(200).json({ currentUser });
+  } catch (err) {
+    console.log(err);
+    res.status(400).end();
+  }
+}
