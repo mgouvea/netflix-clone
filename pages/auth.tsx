a74d5a342de36428937ce375a2598d7c5d67fe3e
Mateus Gouvêa <falamateus.gouvea@gmail.com>
2023-10-14 12:04:45 -0300

    feat(profile): add profile page

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -2,14 +2,11 @@ import Input from '@/components/Input';
 import axios from 'axios';
 import { useCallback, useState } from 'react';
 import { signIn } from 'next-auth/react';
-import { useRouter } from 'next/router';
 
 import { FcGoogle } from 'react-icons/fc';
 import { FaGithub } from 'react-icons/fa';
 
 const Auth = () => {
-  const router = useRouter();
-
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -27,19 +24,14 @@ const Auth = () => {
       const response = await signIn('credentials', {
         email,
         password,
-        redirect: false,
-        callbackUrl: '/',
+        callbackUrl: '/profiles',
       });
-      console.log('req.statusCode', response);
-      if (response!.status === 200) {
-        router.push('/');
-      }
 
       return response;
     } catch (error) {
       console.log(error);
     }
-  }, [email, password, router]);
+  }, [email, password]);
 
   const register = useCallback(async () => {
     try {
@@ -102,14 +94,14 @@ const Auth = () => {
 
             <div className="flex flex-row items-center gap-4 mt-8 justify-center">
               <div
-                onClick={() => signIn('google', { callbackUrl: '/' })}
+                onClick={() => signIn('google', { callbackUrl: '/profiles' })}
                 className="w-10 h-10 bg-white rounded-full flex items-center justify-center 
                                 cursor-pointer hover:opacity-80 transition"
               >
                 <FcGoogle size={30} />
               </div>
               <div
-                onClick={() => signIn('github', { callbackUrl: '/' })}
+                onClick={() => signIn('github', { callbackUrl: '/profiles' })}
                 className="w-10 h-10 bg-white rounded-full flex items-center justify-center 
                                 cursor-pointer hover:opacity-80 transition"
               >
